49a9e4a06e06adb808f8f11b9b4b5e7f2e13ab0a
Jared DiCioccio <jared.dicioccio@gmail.com>
2023-06-26 15:58:11 -0400

    Added update

--- src/main/java/com/jmd/swe645/assignment1/TestController.java
+++ src/main/java/com/jmd/swe645/assignment1/TestController.java
@@ -16,7 +16,7 @@ public class TestController {
 
     @GetMapping("/test")
     public String index() {
-        return "Greetings from Spring Boot hosted on Tomcat!";
+        return "Greetings from Spring Boot hosted on Tomcat via Kubernetes!";
     }
 
 }
